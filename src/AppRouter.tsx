32fe2bb1218a77454b7f31a8ad9ac6f1dcf57cf4
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2021-04-18 21:27:53 -0300

    routes

---
+++ src/AppRouter.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+  
+import Home from "./scenes/Home"
+import RoutingTest from "./scenes/RoutingTest"
+
+export default function AppRouter(){
+    return(
+        <Router>
+          <Switch>
+          <Route path="/routing-test">
+            <RoutingTest />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        </Router>
+    )
+}
