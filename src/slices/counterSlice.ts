54f1af84308fa34c769538b8d070a23869b36c41
Matheus Esteves <matheusesteveszanoto@gmail.com>
2021-04-25 16:26:37 -0300

    remove unecessary dependency for lint in github actions

--- src/slices/counterSlice.ts
+++ src/slices/counterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../store";
 
 // Define a type for the slice state
