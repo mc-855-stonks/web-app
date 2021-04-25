2874aad3fb168f3ffd58a530dd2739c40a8170f0
Matheus Esteves <matheusesteveszanoto@gmail.com>
2021-04-25 16:37:09 -0300

    Add snapshot testing for HomePage

---
+++ src/pages/HomePage.test.tsx
@@ -0,0 +1,9 @@
+import renderer from 'react-test-renderer'
+import HomePage from './HomePage';
+
+test('Home Page should match snapshot', () => {
+  const component = renderer.create(
+    <HomePage></HomePage>
+  )
+  expect(component.toJSON()).toMatchSnapshot()
+})
