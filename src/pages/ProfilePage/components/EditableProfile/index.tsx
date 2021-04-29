44c14e8308620a4f014180ca9294a5fafccf3556
Matheus Esteves <matheusesteveszanoto@gmail.com>
2021-04-29 19:49:29 -0300

    WIP: Editable profile component

--- src/pages/ProfilePage/components/EditableProfile/index.tsx
+++ src/pages/ProfilePage/components/EditableProfile/index.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import stylesEditableProfile from "./style.module.css";
 import stylesProfilePage from "../../style.module.css";
 import Title from "../../../../components/Title";
+import Button from "../../../../components/Button";
 import EditableArea from "./EditableArea";
 
 export default function EditableProfile() {
   return (
     <div className={stylesEditableProfile.container}>
       <Title className={stylesProfilePage.title}>Editar Dados</Title>
-      <EditableArea />
+      <div className={stylesEditableProfile.profileContent}>
+        <EditableArea />
+        <Button value="SALVAR" style={{ width: 189, marginLeft: "auto" }} />
+      </div>
     </div>
   );
 }
--- src/pages/ProfilePage/components/ProductInfo/index.tsx
+++ src/pages/ProfilePage/components/ProductInfo/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
-import styles from "./style.module.css";
+import stylesProductInfo from "./style.module.css";
+import stylesProfilePage from "../../style.module.css";
+import Title from "../../../../components/Title";
 
 export default function ProductInfo() {
-  return <div />;
+  return (
+    <div className={stylesProductInfo.container}>
+      <Title className={stylesProfilePage.title}>Informações</Title>
+    </div>
+  );
 }
