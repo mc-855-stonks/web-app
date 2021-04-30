be3e7760b08ae2a79dd77f049f13c69d42392663
Matheus Esteves <matheusesteveszanoto@gmail.com>
2021-04-30 08:10:21 -0300

    feat: product info component

--- src/pages/ProfilePage/components/ProductInfo/index.tsx
+++ src/pages/ProfilePage/components/ProductInfo/index.tsx
@@ -7,6 +7,62 @@ export default function ProductInfo() {
   return (
     <div className={stylesProductInfo.container}>
       <Title className={stylesProfilePage.title}>Informações</Title>
+      <div className={stylesProductInfo.productContent}>
+        <div className={stylesProductInfo.productInfoGroup}>
+          <Title
+            className={`${stylesProductInfo.productInfoText} ${stylesProductInfo.productInfoTitle}`}
+          >
+            Sobre
+          </Title>
+          <h2
+            className={`${stylesProductInfo.productInfoText} ${stylesProductInfo.productInfoTextAbout}`}
+          >
+            O nomedosite foi desenvolvido para a disciplina MC855 - Projeto em
+            Sistemas de Computação da Universidade Estadual de Campinas
+            (Unicamp) ministrada pelo Professor Doutor Arthur João Catto no 1°
+            semestre de 2021.
+          </h2>
+        </div>
+        <div className={stylesProductInfo.productInfoGroup}>
+          <Title
+            className={`${stylesProductInfo.productInfoText} ${stylesProductInfo.productInfoTitle}`}
+          >
+            Desenvolvedores
+          </Title>
+          <h2
+            className={`${stylesProductInfo.productInfoText} ${stylesProductInfo.productInfoTextAbout}`}
+          >
+            Gabriel Oliveira dos Santos
+          </h2>
+          <h2
+            className={`${stylesProductInfo.productInfoText} ${stylesProductInfo.productInfoTextAbout}`}
+          >
+            João Vitor Araki Gonçalves
+          </h2>
+          <h2
+            className={`${stylesProductInfo.productInfoText} ${stylesProductInfo.productInfoTextAbout}`}
+          >
+            Luma Gabino Vasconcelos
+          </h2>
+          <h2
+            className={`${stylesProductInfo.productInfoText} ${stylesProductInfo.productInfoTextAbout}`}
+          >
+            Matheus Esteves Zanoto
+          </h2>
+        </div>
+        <div className={stylesProductInfo.productInfoGroup}>
+          <Title
+            className={`${stylesProductInfo.productInfoText} ${stylesProductInfo.productInfoTitle}`}
+          >
+            Contato
+          </Title>
+          <h2
+            className={`${stylesProductInfo.productInfoText} ${stylesProductInfo.productInfoTextAbout}`}
+          >
+            nomedosite@gmail.com
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
