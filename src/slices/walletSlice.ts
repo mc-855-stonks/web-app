c13b27f4a41f43a7c58c3e62178ce727a2ef6553
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2021-05-03 22:12:22 -0300

    fix warnings

--- src/slices/walletSlice.ts
+++ src/slices/walletSlice.ts
@@ -1,5 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import type { RootState } from "../store";
+import { createSlice } from "@reduxjs/toolkit";
 
 interface WalletState {
   wip: string;
