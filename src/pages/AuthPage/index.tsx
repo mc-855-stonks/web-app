4dc42b4efa0b815c277bffba9819154aaa40955a
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2021-05-11 22:19:39 -0300

    loading overlay

--- src/pages/AuthPage/index.tsx
+++ src/pages/AuthPage/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { useAppSelector } from "hooks";
+
+import { selectStatus as selectStatusLogin } from "slices/loginSlice";
+import { selectStatus as selectStatusRegister } from "slices/registerSlice";
 
 import LoadingOverlay from "components/LoadingOverlay";
 
@@ -9,9 +13,12 @@ import RegisterCard from "./components/RegisterCard";
 import styles from "./style.module.css";
 
 export default function AuthPage() {
+  const loadingLogin = useAppSelector(selectStatusLogin) === "loading";
+  const loadingRegister = useAppSelector(selectStatusRegister) === "loading";
+
   return (
     <div className={styles.container}>
-      <LoadingOverlay />
+      {(loadingLogin || loadingRegister) && <LoadingOverlay />}
       <Switch>
         <Route path="/login" component={LoginCard} />
         <Route path="/register" component={RegisterCard} />
