bc480f0d056cae8e10044987bc7b4d1dabb99e4b
Luma Gabino Vasconcelos <lumagv@fastshop.com.br>
2021-05-20 12:44:23 -0300

    create landingpage stats section

--- src/pages/LandingPage/components/LandingPageContainer/index.tsx
+++ src/pages/LandingPage/components/LandingPageContainer/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import style from "./style.module.css";
 import Presentation from "../Presentation";
 import Benefits from "../Benefits";
+import Statistics from "../Statistics";
 
 export default function LandingPageContainer() {
   return (
     <div className={style.container}>
       <Presentation />
       <Benefits />
+      <Statistics />
     </div>
   );
 }
