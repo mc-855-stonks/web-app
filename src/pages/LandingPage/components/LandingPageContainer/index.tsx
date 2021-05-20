bae0ee63dee5e3e677a837a0dbb6a81b7605b3bf
Luma Gabino Vasconcelos <lumagv@fastshop.com.br>
2021-05-20 11:21:19 -0300

    create benefits section in landingpage

--- src/pages/LandingPage/components/LandingPageContainer/index.tsx
+++ src/pages/LandingPage/components/LandingPageContainer/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import style from "./style.module.css";
 import Presentation from "../Presentation";
+import Benefits from "../Benefits";
 
 export default function LandingPageContainer() {
   return (
     <div className={style.container}>
       <Presentation />
+      <Benefits />
     </div>
   );
 }
