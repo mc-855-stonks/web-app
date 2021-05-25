db546c0d9fe677b4e38e65188166b13a3c739483
Luma Gabino Vasconcelos <lumagv@fastshop.com.br>
2021-05-25 19:33:59 -0300

    ajusts in right  constraints for screen width

--- src/pages/LandingPage/components/Benefits/index.tsx
+++ src/pages/LandingPage/components/Benefits/index.tsx
@@ -19,17 +19,19 @@ export default function Benefits() {
           remaining essentially unchanged."
           />
         </div>
-        <BenefitItem
-          imageName="image3"
-          titleTop="Comparações com índices"
-          titleBottom="do mercado"
-          description=" Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industrys standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only
-        five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged."
-        />
+        <div className={style.tirdBenefit}>
+          <BenefitItem
+            imageName="image3"
+            titleTop="Comparações com índices"
+            titleBottom="do mercado"
+            description=" Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged."
+          />
+        </div>
       </div>
       <div className={style.secondBenefit}>
         <BenefitItem
