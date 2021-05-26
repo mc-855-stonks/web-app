5410f40676eac51253e9121e25f7b14fbcdf00ea
Luma Gabino Vasconcelos <lumagv@fastshop.com.br>
2021-05-25 22:04:48 -0300

    header section

--- src/pages/LandingPage/components/LandingPageContainer/index.tsx
+++ src/pages/LandingPage/components/LandingPageContainer/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import style from "./style.module.css";
+import Header from "../Header";
 import Presentation from "../Presentation";
 import Benefits from "../Benefits";
 import Statistics from "../Statistics";
@@ -9,6 +10,7 @@ import Footer from "../Footer";
 export default function LandingPageContainer() {
   return (
     <div className={style.container}>
+      <Header />
       <Presentation />
       <Benefits />
       <Statistics />
