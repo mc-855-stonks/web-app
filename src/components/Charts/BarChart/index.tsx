72e4889e42cd8d37c6f3c629bc3a0a690dcecf71
Matheus Esteves <matheusesteveszanoto@gmail.com>
2021-05-29 12:21:16 -0300

    Passing Bar Chart props

--- src/components/Charts/BarChart/index.tsx
+++ src/components/Charts/BarChart/index.tsx
@@ -8,33 +8,41 @@ import {
   Tooltip,
 } from "recharts";
 
-export default function BarChart() {
-  const data = [
-    { name: "Mar/2020", valor: 4000, },
-    { name: "", valor: 3000, },
-    { name: "", valor: -2000, },
-    { name: "", valor: 2780, },
-    { name: "", valor: -300, },
-    { name: "", valor: 2390, },
-    { name: "", valor: 2390, },
-    { name: "", valor: -1500, },
-    { name: "", valor: 500, },
-    { name: "", valor: 1800, },
-    { name: "", valor: -3000, },
-    { name: "Abr/2021", valor: 3490, },
-  ];
+export interface BarChartDatasetPointType {
+  name: string;
+  value: number;
+}
 
+interface Props {
+  dataset: Array<BarChartDatasetPointType>;
+  width: number;
+  height: number;
+  xAxisDataKey: string;
+  yAxisDataKey: string;
+  tooltipName: string;
+  barColor: string;
+}
+
+export default function BarChart({
+  dataset,
+  width,
+  height,
+  xAxisDataKey,
+  yAxisDataKey,
+  tooltipName,
+  barColor,
+}: Props) {
   return (
     <BarChartRecharts
-      width={430}
-      height={266}
-      data={data}
+      width={width}
+      height={height}
+      data={dataset}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
+      <XAxis dataKey={xAxisDataKey} />
+      <YAxis dataKey={yAxisDataKey} />
       <Tooltip />
-      <Bar dataKey="valor" fill="#A5C1CE" />
+      <Bar dataKey={yAxisDataKey} name={tooltipName} fill={barColor} />
     </BarChartRecharts>
   );
 }
--- src/pages/DashboardPage/components/Dashboard/components/Performance/components/MonthlyIncome/index.tsx
+++ src/pages/DashboardPage/components/Dashboard/components/Performance/components/MonthlyIncome/index.tsx
@@ -6,12 +6,35 @@ import incomeStyle from "./style.module.css";
 import TimeSelector from "../TimeSelector";
 
 export default function MonthlyIncome() {
+  const data = [
+    { name: "Mar/2020", value: 4000, },
+    { name: "", value: 3000, },
+    { name: "", value: -2000, },
+    { name: "", value: 2780, },
+    { name: "", value: -300, },
+    { name: "", value: 2390, },
+    { name: "", value: 2390, },
+    { name: "", value: -1500, },
+    { name: "", value: 500, },
+    { name: "", value: 1800, },
+    { name: "", value: -3000, },
+    { name: "Abr/2021", value: 3490, },
+  ];
+
   return (
     <div className={dashboardStyle.card}>
       <h2 className={performanceStyle.chartTitle}>Rendimento Mensal (R$)</h2>
       <TimeSelector />
       <div className={incomeStyle.barChartContainer}>
-        <BarChart />
+        <BarChart
+          dataset={data}
+          width={430}
+          height={266}
+          xAxisDataKey="name"
+          yAxisDataKey="value"
+          tooltipName="Valor"
+          barColor="#A5C1CE"
+        />
       </div>
     </div>
   );
