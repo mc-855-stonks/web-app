54d73b7869122d247ab1b1e46c4082d90f28ca85
Matheus Esteves <matheusesteveszanoto@gmail.com>
2021-05-30 11:25:55 -0300

    Recharts line chart component with props

---
+++ src/components/Charts/LineChart/index.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  LineChart as LineChartRecharts,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+
+export interface LineData {
+  dataKey: string;
+  name: string;
+  color: string;
+}
+
+interface Props {
+  dataset: Array<any>;
+  xAxisDataKey: string;
+  lineDataList: Array<LineData>;
+  width: number;
+  height: number;
+  tickFormatter: any;
+}
+
+export default function LineChart({
+  dataset,
+  xAxisDataKey,
+  lineDataList,
+  width,
+  height,
+  tickFormatter,
+}: Props) {
+  return (
+    <LineChartRecharts
+      width={width}
+      height={height}
+      data={dataset}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey={xAxisDataKey} />
+      <YAxis tickFormatter={tickFormatter} />
+      <Tooltip />
+      {lineDataList.map((lineData) => 
+        <Line
+          type="monotone"
+          dataKey={lineData.dataKey}
+          stroke={lineData.color}
+          name={lineData.name}
+          dot={false}
+          strokeWidth={2}
+        />
+      )}
+    </LineChartRecharts>
+  );
+}
--- src/pages/DashboardPage/components/Dashboard/components/Performance/components/Benchmark/index.tsx
+++ src/pages/DashboardPage/components/Dashboard/components/Performance/components/Benchmark/index.tsx
@@ -1,9 +1,86 @@
 import React from "react";
 import dashboardStyle from "pages/DashboardPage/components/Dashboard/style.module.css";
 import performanceStyle from "pages/DashboardPage/components/Dashboard/components/Performance/style.module.css";
+import LineChart from "components/Charts/LineChart";
+import benchmarkStyle from "./style.module.css";
 import TimeSelector from "../TimeSelector";
 
 export default function Benchmark() {
+  const data = [
+    {
+      name: "Abr/2020",
+      ibov: 40,
+      cdi: 24,
+      ipca: 24,
+    },
+    {
+      name: "",
+      ibov: 20,
+      cdi: 10,
+      ipca: 15,
+    },
+    {
+      name: "",
+      ibov: 19,
+      cdi: 31,
+      ipca: 44,
+    },
+    {
+      name: "",
+      ibov: 66,
+      cdi: 13,
+      ipca: 58,
+    },
+    {
+      name: "",
+      ibov: 98,
+      cdi: 5,
+      ipca: 10,
+    },
+    {
+      name: "",
+      ibov: 80,
+      cdi: 10,
+      ipca: 16,
+    },
+    {
+      name: "",
+      ibov: 29,
+      cdi: 7,
+      ipca: 67,
+    },
+    {
+      name: "",
+      ibov: 20,
+      cdi: 98,
+      ipca: 21,
+    },
+    {
+      name: "",
+      ibov: 27,
+      cdi: 39,
+      ipca: 20,
+    },
+    {
+      name: "",
+      ibov: 18,
+      cdi: 48,
+      ipca: 21,
+    },
+    {
+      name: "",
+      ibov: 23,
+      cdi: 38,
+      ipca: 25,
+    },
+    {
+      name: "Abr/2021",
+      ibov: 34,
+      cdi: 43,
+      ipca: 21,
+    },
+  ];
+
   return (
     <div className={dashboardStyle.card}>
       <h2 className={performanceStyle.chartTitle}>Benchmark</h2>
@@ -13,6 +90,20 @@ export default function Benchmark() {
         selectSixMonths={() => { }}
         selectThreeMonths={() => { }}
       />
+      <div className={benchmarkStyle.lineChartContainer}>
+        <LineChart
+          dataset={data}
+          width={430}
+          height={220}
+          xAxisDataKey="name"
+          lineDataList={[
+            { dataKey: "ibov", name: "IBOV", color: "#E8C603", },
+            { dataKey: "cdi", name: "CDI", color: "#3F84BF", },
+            { dataKey: "ipca", name: "IPCA", color: "#A5C1CE", },
+          ]}
+          tickFormatter={(tick: any) => { return `${tick}%`; }}
+        />
+      </div>
     </div>
   );
 }
