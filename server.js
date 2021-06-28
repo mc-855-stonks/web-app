c45a08e149bc0274cefc659bc3d80cff9d07dd3f
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2021-06-28 20:07:33 -0300

    fix server favicon

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + "/build/favicon.png"));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/healthcheck', function (req, res) {
- return res.send('ok');
+app.get("/healthcheck", function (req, res) {
+  return res.send("ok");
 });
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port);
